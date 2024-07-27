b62d1a6646a5635c6d5a0db5ed7003445567e70d
Krushit-Babariya <krushitsbabariya41@gmial.com>
2024-07-27 11:32:40 +0530

    add() method added

--- MavenProj01/src/main/java/com/krushit/apps/App.java
+++ MavenProj01/src/main/java/com/krushit/apps/App.java
@@ -5,5 +5,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Additioon is :: " + add(10,20));
+    }
+    
+    public static int add(int a, int b) {
+    	return a+b;
     }
 }
